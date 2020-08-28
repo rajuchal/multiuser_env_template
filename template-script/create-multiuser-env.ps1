aa90b71110780e80a3ac8a4b723ea25fe4971d3f
rajuchal <69861082+rajuchal@users.noreply.github.com>
2020-08-28 22:19:51 +0530

    2.0

--- template-script/create-multiuser-env.ps1
+++ template-script/create-multiuser-env.ps1
@@ -1,10 +1,15 @@
-$projectName = Read-Host -Prompt "Enter the same project name"
-$adminUsername = Read-Host -Prompt "Enter the administrator User name"
-$adminPassword = Read-Host -Prompt "Enter the administrator password" -AsSecureString
+$projectName = Read-Host -Prompt "Enter the some project/Resource Group name"
 $templateFile = Read-Host -Prompt "Enter the full path of template file location"
+$parameterFile = Read-Host -Prompt "Enter the full path of parameter file location"
 $noOfUsers = Read-Host -Prompt "Enter the number of Users"
 $resourceGroupName = "${projectName}_rg"
 
 New-AzResourceGroup -Name $resourceGroupName -Location "East US"
 
-New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $templateFile -adminUsername $adminUsername -adminPassword $adminPassword -no_of_win_vm $noOfUsers -no_of_linux_vm $noOfUsers -Verbose
+New-AzResourceGroupDeployment `
+	-ResourceGroupName $resourceGroupName `
+	-TemplateFile $templateFile `
+	-TemplateParameterFile $parameterFile `
+	-no_of_win_vm $noOfUsers `
+	-no_of_linux_vm $noOfUsers `
+	-Verbose
